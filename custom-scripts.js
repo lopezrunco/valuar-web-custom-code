b334fd55e2c346c5168203693e6d68946375f661
Damián López <lopezrunco@gmail.com>
2023-04-06 11:20:12 +0530

    menu

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,1 +1,26 @@
-console.log('testing custom-scripts.js')
+console.log('Running custom JS')
+let d = document
+
+d.addEventListener('DOMContentLoaded', (e) => {
+    appendContactInfo()
+})
+
+// Insertar info de contacto en header
+const appendContactInfo = () => {
+    const $contactInfoWrapper = d.createElement('div')
+    const headerElement = d.getElementById('astroid-header')
+
+    $contactInfoWrapper.classList.add('contact-info-wrapper')
+    $contactInfoWrapper.innerHTML = `
+        <div class='container'>
+            <div class='row'>
+                <div class='content-wrapper'>
+                    <a href='https://wa.me/59896591693' target='_blank'><i class="fab fa-whatsapp"></i>096 591 693</a>
+                    <a href='tel:26011774' target='_blank'><i class="fas fa-phone"></i>2601 1774</a>
+                    <a href='mailto:info@valuar.com.uy' target='_blank'><i class="far fa-envelope"></i>info@valuar.com.uy</a>
+                </div>
+            </div>
+        </div>
+    `
+    headerElement.appendChild($contactInfoWrapper)
+}
